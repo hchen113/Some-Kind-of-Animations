21b5e7806346a4fe463bf2b95ed342c08d3ea91b
Henry Chen <hchen113@binghamton.edu>
2019-09-23 10:19:14 -0400

    Initial Commit

---
+++ Programming Assignment 03/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Programming Assignment 03
+//
+//  Created by Henry Chen on 9/23/19.
+//  Copyright © 2019 Henry Chen. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Programming Assignment 03Tests/Programming_Assignment_03Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Programming_Assignment_03Tests.swift
+//  Programming Assignment 03Tests
+//
+//  Created by Henry Chen on 9/23/19.
+//  Copyright © 2019 Henry Chen. All rights reserved.
+//
+
+import XCTest
+@testable import Programming_Assignment_03
+
+class Programming_Assignment_03Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
